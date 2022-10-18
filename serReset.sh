8065507d40a0a73433179aa2084807385cbab8fd
n3on <97998625+n3onhacks@users.noreply.github.com>
2022-10-18 01:00:37 -0400

    Create serReset.sh

---
+++ serReset.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "next chat explosion in 30 minutes" > status.txt
+
+sleep 10m
+echo "next chat explosion in 20 minutes" > status.txt
+
+sleep 10m
+echo "next chat explosion in 10 minutes" > status.txt
+
+sleep 1m
+echo "next chat explosion in 9 minutes" > status.txt
+
+sleep 1m
+echo "next chat explosion in 8 minutes" > status.txt
+
+sleep 1m
+echo "next chat explosion in 7 minutes" > status.txt
+
+
+sleep 1m
+echo "next chat explosion in 6 minutes" > status.txt
+
+
+sleep 1m
+echo "next chat explosion in 5 minutes" > status.txt
+
+
+sleep 1m
+echo "next chat explosion in 4 minutes" > status.txt
+
+
+sleep 1m
+echo "next chat explosion in 3 minutes" > status.txt
+
+
+sleep 1m
+echo "next chat explosion in 2 minutes" > status.txt
+
+
+sleep 1m
+echo "next chat explosion in 1 minutes" > status.txt
+
+echo "" > chat.txt
+echo "" > chat1.txt
+echo "" > outgoing/chat.txt
+echo "START NEW CHAT - PREVIOUS ONE DESTROYED." > status.txt
